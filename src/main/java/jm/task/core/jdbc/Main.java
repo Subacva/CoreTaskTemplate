26a40c1db848d46ce0b5912d19be6593edbff758
Vitaliy Ledovskoy <superpups2003@gmail.com>
2021-11-24 02:06:41 +0300

    commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,20 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Dmitry", "Dabaf", (byte) 28);
+        userService.saveUser("Kola", "Vertor", (byte) 14);
+        userService.saveUser("Damo", "Chiki", (byte) 59);
+        userService.saveUser("Zece", "Cefe", (byte) 42);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
+
